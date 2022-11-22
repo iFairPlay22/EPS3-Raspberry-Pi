fee50c29bbf4e27ee748cbdccfdfd0705a21527d
Ewen Bouquet <ewen.bouquet@gmail.com>
2022-11-22 18:27:19 +0100

    Comments v3

--- python/thermal_imaging/__init__.py
+++ python/thermal_imaging/__init__.py
@@ -12,18 +12,26 @@ class ThermalCamera():
         print("MLX addr detected on I2C", [hex(i) for i in self.mlx.serial_number])
 
     def get(self):
+
+        # Dimention of the thermal camera image
         size   = (24, 32)
+        
+        # We will store the temperature inside this matrix
         mat    = [[0 for i in range(size[1])] for j in range(size[0])]
         
+        # Temporary list, that will store the data of the thermal camera
         buffer = [0] * (size[0] * size[1])
 
+        # Wait to have a frame
         while True:
 
+            # When he have it
             try:
                 self.mlx.getFrame(buffer)
             except ValueError:
                 continue
 
+            # Store the data in the matrix
             for h in range(size[0]):
                 for w in range(size[1]):
                     mat[h][w] = buffer[h*size[1]+w]
